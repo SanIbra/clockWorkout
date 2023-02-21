8755bff0c91cd95a703fc7e4f38b5079758dd235
Ibrahim <ibrakia7@gmail.com>
2023-02-21 23:35:16 +0100

    Amelioration visuel

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+const mainColor = 'green';
 
 const styles = StyleSheet.create({
   container: {
@@ -21,61 +22,97 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 10,
     borderRadius: 100,
-    backgroundColor: 'orange',
+
+    textAlign: 'center',
+    backgroundColor: mainColor,
+    mainColor: 'black'
   },
 });
 
 export default function App() {
+  console.log("start")
   const timerIniatialValue = 40
 
   const [timer, setTimer] = useState(timerIniatialValue);
 
   const [isRuning, setIsRuning] = useState(false);
 
+  const [timerTitle, setTimerTitle] = useState("");
+
+  const majTimerTitle = () => setTimerTitle(timer + '\n' + (isRuning ? 'Pause' : 'Start'))
   setTimeout(() => {
     if (isRuning) {
       setTimer(timer - 1);
+      majTimerTitle();
     }
     return;
   }, 1000)
 
   const reset = () => {
     setTimer(timerIniatialValue);
+    majTimerTitle();
   }
 
   const add10s = () => {
     setTimer(timer + 10);
+    majTimerTitle();
   }
-
+  // majTimerTitle();
 
   return (<View
     style={[styles.container]}>
     <View style={{ flex: 1, backgroundColor: 'red' }} >
       <Text> Top de l'application</Text>
     </View>
-    <View style={{ flex: 4, backgroundColor: 'darkorange', justifyContent: 'center', textAlign: 'center', }} >
-      <Text style={{ textAlign: 'center' }}> {timer}</Text>
-      <Button
-        onPress={() => {
-          setIsRuning(!isRuning);
-        }}
-        title={isRuning ? 'Pause' : 'Start'}>
-      </Button>
+    <View style={{ flex: 4, justifyContent: 'center', backgroundColor: 'darkorange' }} >
+      <View style={{ flex: 1, flexDirection: 'row', backgroundColor: 'rose', justifyContent: 'center' }}>
+        <RoundButton
+          style={{
+            width: 200,
+            height: 200
+          }}
+          onPress={() => setIsRuning(!isRuning)}
+          title={timerTitle}
+        />
+        {/* <TouchableOpacity
+          style={[styles.roundButton1, {
+            width: 200,
+            height: 200,
+            textAlign: 'center',
+            backgroundColor: mainColor,
+            mainColor: 'black'
+          }]}
+          onPress={() => {
+            setIsRuning(!isRuning);
+          }}>
+          <Text style={{ color: 'black', textAlign: 'center' }}>{timer}{'\n'}{isRuning ? 'Pause' : 'Start'}</Text>
+        </TouchableOpacity> */}
+      </View>
       <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
-        <Button
-          style={styles.roundButton1}
+        <RoundButton
           onPress={add10s}
-          title='+10s'>
-        </Button>
-        <Button
-          style={{}}
+          title='+10s'
+        />
+        <RoundButton
           onPress={reset}
-          title='reset'>
-        </Button>
+          title='reset'
+        />
       </View>
     </View>
     <View style={{ flex: 1, backgroundColor: 'green' }} >
       <Text>Bottom de l'application</Text>
     </View>
   </View>)
+}
+
+
+const RoundButton = (props) => {
+  const style = props.style ? props.style : {};
+  return (
+    <TouchableOpacity
+      style={[styles.roundButton1, style]}
+      onPress={props.onPress}>
+      <Text style={{ color: 'black', textAlign: 'center' }}>{props.title}</Text>
+    </TouchableOpacity>
+  )
 }
